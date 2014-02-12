7716a39bbcb4f534a1ff26d413b6b6a8465f0794
Kevin <kevinwarde@gmail.com>
2014-02-12 02:00:39 +0000

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gnt::Application.config.secret_key_base = 'cf884956fb67435c567598814720588ca6b3598c9ac8615cc2d39207faf90ca4467aeecfceca940d9fa61c1adcb10b102ae4f4e291d1c26bb16fa7b89bb9cf0e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gnt::Application.config.session_store :cookie_store, key: '_gnt_session'
