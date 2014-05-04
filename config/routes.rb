ce711774271e0589218f98174b10b37432a0be82
Kevin <kevinwarde@gmail.com>
2014-05-04 12:27:03 +0000

    last commit of old changes when picked up again on may 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Gnt::Application.routes.draw do
   resources :hosts
 
-  resources :games
+  resources :games do 
+    resources :hosts
+  end
+
   root to: 'games#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
